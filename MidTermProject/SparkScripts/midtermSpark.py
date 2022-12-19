0fdf00b8f7c102b584d3b7b8b2f798889b28bf40
Ubuntu <ubuntu@ip-172-31-28-14.ec2.internal>
2022-12-19 08:33:43 +0000

    Upload midterm Code

---
+++ MidTermProject/SparkScripts/midtermSpark.py
@@ -0,0 +1,199 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, LongType, FloatType, DateType, StringType, VarcharType, BooleanType, DecimalType
+from pyspark.sql.functions import col, sum as SUM, avg, unix_timestamp, to_date, date_format, coalesce, lit, when, count
+import json
+import argparse
+
+
+class SparkRunner:
+
+    def __init__(self, spark_name, input_file_url) -> None:
+        self.spark_name = spark_name
+        self.input_file_url = input_file_url
+        self.input_file_dict = json.loads(self.input_file_url)
+        self.dateStr = self.input_file_dict.keys()[0].split("/")[1]
+        self.output_file_url = "s3://hui-mid-term/output/"
+        self.spark = self.spark_init(spark_name)
+
+    def spark_init(self, spark_name):
+        spark = SparkSession.builder.master("yarn").appName(spark_name).getOrCreate()
+        return spark
+
+    def etl(self, spark):
+        calendar_schema= [
+            StructField("CAL_DT", DateType(), False), \
+            StructField("CAL_TYPE_DESC", StringType(), False), \
+            StructField("DAY_OF_WK_NUM", IntegerType(), False), \
+            StructField("DAY_OF_WK_DESC", StringType(), False), \
+            StructField("YR_NUM", IntegerType(), False), \
+            StructField("WK_NUM", IntegerType(), False), \
+            StructField("YR_WK_NUM", IntegerType(), False), \
+            StructField("MNTH_NUM", IntegerType(), False), \
+            StructField("YR_MNTH_NUM", IntegerType(), False), \
+            StructField("QTR_NUM", IntegerType(), False), \
+            StructField("YR_QTR_NUM", IntegerType(), False)
+        ]
+        calendar_struc = StructType(fields=calendar_schema)
+        calendar_dim = spark.read.csv(f's3a://{self.input_file_dict["calendar.csv"]}', header=True, schema=calendar_struc)
+
+        store_schema= [
+            StructField("STORE_KEY", IntegerType(), False), \
+            StructField("STORE_NUM", StringType(), False), \
+            StructField("STORE_DESC", StringType(), False), \
+            StructField("ADDR", StringType(), False), \
+            StructField("CITY", StringType(), False), \
+            StructField("REGION", StringType(), False), \
+            StructField("CNTRY_CD", StringType(), False), \
+            StructField("CNTRY_NM", StringType(), False), \
+            StructField("POSTAL_ZIP_CD", StringType(), False), \
+            StructField("PROV_STATE_CD", StringType(), False), \
+            StructField("PROV_STATE_DESC", StringType(), False), \
+            StructField("STORE_TYPE_CD", StringType(), False),\
+            StructField("STORE_TYPE_DESC", StringType(), False), \
+            StructField("FRNCHS_FLG", BooleanType(), False), \
+            StructField("STORE_SIZE", DecimalType(), False), \
+            StructField("MARKET_KEY", IntegerType(), False),\
+            StructField("MARKET_NAME", StringType(), False), \
+            StructField("SUBMARKET_KEY", IntegerType(), False), \
+            StructField("SUBMARKET_NAME", StringType(), False), \
+            StructField("LATITUDE", DecimalType(19, 6), False),\
+            StructField("LONGITUDE", DecimalType(19, 6), False)
+        ]
+        store_struc = StructType(fields=store_schema)
+        store_dim = spark.read.csv(f's3a://{self.input_file_dict["store.csv"]}', header=True, schema=store_struc)
+
+        product_schema= [
+            StructField("PROD_KEY", IntegerType(), False), \
+            StructField("PROD_NAME", StringType(), False), \
+            StructField("VOL", DecimalType(38, 2), False), \
+            StructField("BRAND_NAME", StringType(), False), \
+            StructField("WGT", DecimalType(38, 2), False), \
+            StructField("STATUS_CODE", StringType(), False), \
+            StructField("STATUS_CODE_NAME", StringType(), False), \
+            StructField("CATEGORY_KEY", IntegerType(), False), \
+            StructField("CATEGORY_NAME", StringType(), False), \
+            StructField("SUBCATEGORY_KEY", IntegerType(), False), \
+            StructField("SUBCATEGORY_NAME", StringType(), False)
+        ]
+        product_struc = StructType(fields=product_schema)
+        product_dim = spark.read.csv(f's3a://{self.input_file_dict["product.csv"]}', header=True, schema=product_struc)       
+
+        sales_schema= [
+            StructField("TRANS_ID", DateType(), False), \
+            StructField("PROD_KEY", IntegerType(), False), \
+            StructField("STORE_KEY", IntegerType(), False), \
+            StructField("TRANS_DT", DateType(), False), \
+            StructField("TRANS_TIME", IntegerType(), False), \
+            StructField("SALES_QTY", DecimalType(38, 2), False), \
+            StructField("SALES_PRICE", DecimalType(38, 2), False), \
+            StructField("SALES_AMT", DecimalType(38, 2), False), \
+            StructField("DISCOUNT", DecimalType(38, 2), False), \
+            StructField("SALES_COST", DecimalType(38, 2), False), \
+            StructField("SALES_MGRN", DecimalType(38, 2), False), \
+            StructField("SHIP_COST", DecimalType(38, 2), False)
+        ]
+        sales_struc = StructType(fields=sales_schema)
+        sales_dim = spark.read.csv(f's3a://{self.input_file_dict["sales.csv"]}', header=True, schema=sales_struc)
+
+        inventory_schema= [
+            StructField("CAL_DT", DateType(), False), \
+            StructField("STORE_KEY", IntegerType(), False), \
+            StructField("PROD_KEY", IntegerType(), False), \
+            StructField("INVENTORY_ON_HAND_QTY", DecimalType(38, 2), False), \
+            StructField("INVENTORY_ON_ORDER_QTY", DecimalType(38, 2), False), \
+            StructField("OUT_OF_STOCK_FLG", IntegerType(), False), \
+            StructField("WASTE_QTY", DecimalType(38, 2), False), \
+            StructField("PROMOTION_FLG", BooleanType(), False), \
+            StructField("NEXT_DELIVERY_DT", DateType(), False)
+        ]
+        inventory_struc = StructType(fields=inventory_schema)
+        inventory_dim = spark.read.csv(f's3a://{self.input_file_dict["inventory.csv"]}', header=True, schema=inventory_struc)
+
+        daily_sales_stg = sales_dim.groupBy("TRANS_DT", "STORE_KEY", "PROD_KEY").agg(SUM("SALES_QTY").alias("sales_qty"),\
+                                                                                    SUM("SALES_AMT").alias("sales_amt"),\
+                                                                                    avg("SALES_PRICE").alias("sales_price"),\
+                                                                                    SUM("SALES_COST").alias("sales_cost"),\
+                                                                                    SUM("SALES_MGRN").alias("sales_mgrn"),\
+                                                                                    avg("DISCOUNT").alias("discount"),\
+                                                                                    SUM("SHIP_COST").alias("ship_cost")
+                                                                                    )\
+                                                                                .withColumnRenamed("TRANS_DT", "cal_dt")\
+                                                                                .withColumnRenamed("STORE_KEY", "store_key")\
+                                                                                .withColumnRenamed("PROD_KEY", "prod_key")\
+                                                                                .orderBy("cal_dt", "store_key", "prod_key")
+        
+        daily_inventory_stg = inventory_dim.withColumnRenamed("CAL_DT", "cal_dt")\
+                .withColumnRenamed("STORE_KEY", "store_key")\
+                .withColumnRenamed("PROD_KEY", "prod_key")\
+                .withColumnRenamed("INVENTORY_ON_HAND_QTY", "stock_on_hand_qty")\
+                .withColumnRenamed("INVENTORY_ON_ORDER_QTY", "ordered_stock")\
+                .withColumnRenamed("OUT_OF_STOCK_FLG", "out_of_stock_flg")\
+                .withColumnRenamed("WASTE_QTY", "waste_qty")\
+                .withColumnRenamed("PROMOTION_FLG", "promotion_flg")\
+                .withColumnRenamed("NEXT_DELIVERY_DT", "next_delivery_dt")
+
+        sales_inv_store_dy = daily_sales_stg.join(daily_inventory_stg, ['cal_dt','store_key','prod_key'], how='fullouter')\
+            .select(coalesce(daily_sales_stg.cal_dt, daily_inventory_stg.cal_dt).alias('cal_dt'),\
+                    coalesce(daily_sales_stg.store_key, daily_inventory_stg.store_key).alias('store_key'),\
+                    coalesce(daily_sales_stg.prod_key, daily_inventory_stg.prod_key).alias('prod_key'),\
+                    coalesce(daily_sales_stg.sales_qty, lit(0)).alias('sales_qty'),\
+                    coalesce(daily_sales_stg.sales_price, lit(0)).alias('sales_price'),\
+                    coalesce(daily_sales_stg.sales_amt, lit(0)).alias('sales_amt'),\
+                    coalesce(daily_sales_stg.discount, lit(0)).alias('discount'),\
+                    coalesce(daily_sales_stg.sales_cost, lit(0)).alias('sales_cost'),\
+                    coalesce(daily_sales_stg.sales_mgrn, lit(0)).alias('sales_mgrn'),\
+                    coalesce(daily_inventory_stg.stock_on_hand_qty, lit(0)).alias('stock_on_hand_qty'),\
+                    coalesce(daily_inventory_stg.ordered_stock, lit(0)).alias('ordered_stock_qty'),\
+                    coalesce(daily_inventory_stg.out_of_stock_flg, lit(False)).alias('out_of_stock_flg'))
+        sales_inv_store_dy = sales_inv_store_dy.withColumn("in_stock_flg", when(sales_inv_store_dy.out_of_stock_flg == True, False).otherwise(True))
+        sales_inv_store_dy = sales_inv_store_dy.withColumn("low_stock_flg", when(sales_inv_store_dy.stock_on_hand_qty < sales_inv_store_dy.sales_qty, True).otherwise(False))
+
+        sales_inv_store_wk = sales_inv_store_dy.join(calendar_dim, ['cal_dt'], how='inner')
+        sales_inv_store_wk = sales_inv_store_wk.groupBy("YR_NUM", "WK_NUM", "store_key", "prod_key")\
+            .agg(SUM("sales_qty").alias("wk_sales_qty"),\
+                avg("sales_price").alias("avg_sales_price"),\
+                SUM("sales_amt").alias("wk_sales_amt"),\
+                SUM("discount").alias("wk_discount"),\
+                SUM("sales_cost").alias("wk_sales_cost"),\
+                SUM("sales_mgrn").alias("wk_sales_mgrn"),\
+                SUM(when(calendar_dim["DAY_OF_WK_NUM"] == 6, sales_inv_store_dy["stock_on_hand_qty"]).otherwise(0)).alias("eop_stock_on_hand_qty"),\
+                SUM(when(calendar_dim["DAY_OF_WK_NUM"] == 6, sales_inv_store_dy["ordered_stock_qty"]).otherwise(0)).alias("eop_ordered_stock_qty"),\
+                count(when(sales_inv_store_dy["out_of_stock_flg"] == True, 1)).alias("out_of_stock_times"),\
+                count(when(sales_inv_store_dy["in_stock_flg"] == True, 1)).alias("in_stock_times"),\
+                count(when(sales_inv_store_dy["low_stock_flg"] == True, 1)).alias("low_stock_times")
+                )
+        sales_inv_store_wk = sales_inv_store_wk.withColumnRenamed("YR_NUM","yr_num")\
+                .withColumnRenamed("WK_NUM","wk_num")\
+                .orderBy("yr_num", "wk_num", "store_key", "prod_key")
+
+        return (sales_inv_store_wk, store_dim, product_dim, calendar_dim)
+
+    def writeCSV(self, df, output_file_url):
+        df.write.option("header", "True").mode("overwrite").csv(output_file_url)
+    
+    def writeParquet(self, df, output_file_url):
+        df.write.mode("overwrite").parquet("output_file_url")
+
+    def runJob(self):
+        sales_inv_store_wk, store_dim, product_dim, calendar_dim = self.etl(self.spark)
+        # 
+        self.writeParquet(sales_inv_store_wk, f"{self.output_file_url}{self.dateStr}/")
+        self.writeCSV(store_dim, f"{self.output_file_url}{self.dateStr}/store_dim.csv")
+        self.writeCSV(product_dim, f"{self.output_file_url}{self.dateStr}/product_dim.csv")
+        self.writeCSV(calendar_dim, f"{self.output_file_url}{self.dateStr}/calendar_dim.csv")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--spark_name', help="spark_name")
+    parser.add_argument('--input_file_url', help="input file url json string")
+
+    args = parser.parse_args()
+    spark_name = args.spark_name
+    input_file_url = args.input_file_url
+
+    spark_runner = SparkRunner(spark_name, input_file_url)
+    spark_runner.runJob()
+
+# command to run the script in local:
+# spark-submit --master local --deploy-mode client spark.py --spark_name 'airflow_lab' --input_file_url './data/orders_amount.csv' --output_file_url './data/orders_amount_output' --avg_order_amount '29171.860335'
+# spark-submit --master yarn --deploy-mode cluster s3://hui-mid-term/midtermSpark.py --spark_name 'midtermProject' --input_file_url '{"calendar.csv": "input/2022-12-19/calendar.csv","inventory.csv": "input/2022-12-19/inventory.csv","product.csv": "input/2022-12-19/product.csv","sales.csv": "input/2022-12-19/sales.csv","store.csv": "input/2022-12-19/store.csv"}'
